f6e41a91d7bd30fc2ae717cb27beddef93b7ce80
Miguel Muran <muranmiguel@gmail.com>
2026-04-04 09:03:05 -0300

    Configura conexão com banco usando SQLAlchemy

--- sprint_1/back-end/database.py
+++ sprint_1/back-end/database.py
@@ -1,16 +1,20 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./hairtime.db"
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+DATABASE_URL = (
+
+    f"postgresql://{os.getenv('DB_user')}:"  #Busca user .env 
+    f"{os.getenv('DB_password')}@" #Busca senha .env
+    f"{os.getenv('DB_host')}:" #Busca endereço .env
+    f"{os.getenv('DB_port')}/" #Busca porta conexão .env
+    f"{os.getenv('DB_name')}" #Busca nome no .env
+    )
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
